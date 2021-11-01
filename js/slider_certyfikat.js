243c3618bfabd404ab83292d2965308f2bd77f02
Nikita <ilchuk.nikita@gmail.com>
2021-11-01 19:21:07 +0100

    V0.4 Almost finish make first page, add new style, script, img. Start make new page

--- js/slider_certyfikat.js
+++ js/slider_certyfikat.js
@@ -19,3 +19,24 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "flex";  
 }
+
+
+
+function plusSlider(n) {
+  showSlider(slideIndex += n);
+}
+
+function currentSlider(n) {
+  showSlider(slideIndex = n);
+}
+
+function showSlider(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlider");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none"; 
+  }
+  slides[slideIndex-1].style.display = "block"; 
+}
